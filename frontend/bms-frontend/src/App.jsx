a7b0eff5177086de42038f2b2ef7adbeb5347099
ankitRaghuwanshi <ankitraghuwanshi96@gmail.com>
2026-02-13 18:52:30 +0530

    dynamic import using lazy,suspense

--- frontend/bms-frontend/src/App.jsx
+++ frontend/bms-frontend/src/App.jsx
@@ -1,47 +1,70 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import Home from './pages/Home'
-import Register from './pages/Register'
-import Login from './pages/Login'
+import React, { Suspense } from 'react'
 import store from './redux/store'
-import ProtectedRoute from './pages/ProtectedRoute'
-import Admin from './pages/Admin'
-import Partner from './pages/Partner'
-import SingleMovie from './pages/SingleMovie'
 import Loader from './pages/Loader'
-import BookShow from './pages/BookShow'
-import Forget from './pages/Forget'
-import Reset from './pages/Reset'
 
-import { loadStripe } from '@stripe/stripe-js';
-import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js'
 
 const stripePromise = loadStripe('pk_test_51SzNDAQjPKa2CLHXObHZkCZQiIv6swxZzmhY1VzhiwmyFyKo4u8k1iL3sGv1yX16wLxmt0UUR1XdYeckrUwCHCTl00KAOBxVte');
 
+// Lazy loaded pages
+const Home = React.lazy(() => import('./pages/Home'))
+const Register = React.lazy(() => import('./pages/Register'))
+const Login = React.lazy(() => import('./pages/Login'))
+const ProtectedRoute = React.lazy(() => import('./pages/ProtectedRoute'))
+const Admin = React.lazy(() => import('./pages/Admin'))
+const Partner = React.lazy(() => import('./pages/Partner'))
+const SingleMovie = React.lazy(() => import('./pages/SingleMovie'))
+const BookShow = React.lazy(() => import('./pages/BookShow'))
+const Forget = React.lazy(() => import('./pages/Forget'))
+const Reset = React.lazy(() => import('./pages/Reset'))
 
 function App() {
-
   return (
     <Provider store={store}>
-      <Loader/>
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<ProtectedRoute><Home /></ProtectedRoute>}/>
-          <Route path='/admin' element={<ProtectedRoute><Admin /></ProtectedRoute>}/>
-          <Route path='/partner' element={<ProtectedRoute><Partner/></ProtectedRoute>}/>
-          <Route path="/movie/:id" element={<ProtectedRoute><SingleMovie/></ProtectedRoute>}/>
-          <Route path="/book-show/:id" element={
-            <ProtectedRoute>
-              <Elements stripe={stripePromise}>
-                <BookShow />
-              </Elements>
-            </ProtectedRoute>}
-          />
-          <Route path='/register' element={<Register />}/>
-          <Route path='/login' element={<Login />}/>
-          <Route path='/forget' element={<Forget />} />
-          <Route path='/reset' element={<Reset />} />
-        </Routes>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path='/' element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }/>
+
+            <Route path='/admin' element={
+              <ProtectedRoute>
+                <Admin />
+              </ProtectedRoute>
+            }/>
+
+            <Route path='/partner' element={
+              <ProtectedRoute>
+                <Partner/>
+              </ProtectedRoute>
+            }/>
+
+            <Route path="/movie/:id" element={
+              <ProtectedRoute>
+                <SingleMovie/>
+              </ProtectedRoute>
+            }/>
+
+            <Route path="/book-show/:id" element={
+              <ProtectedRoute>
+                <Elements stripe={stripePromise}>
+                  <BookShow />
+                </Elements>
+              </ProtectedRoute>
+            }/>
+
+            <Route path='/register' element={<Register />}/>
+            <Route path='/login' element={<Login />}/>
+            <Route path='/forget' element={<Forget />} />
+            <Route path='/reset' element={<Reset />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </Provider>
   )
